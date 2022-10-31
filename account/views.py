c814654dcfb80faceeb5868aa11a4395ad035a6b
Naveed Khan <naveed.khan@geniobits.in>
2022-10-31 11:16:00 +0530

    initial_commit

---
+++ account/views.py
@@ -0,0 +1,19 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import UserRegistrationSerializer
+
+# Create your views here.
+
+class UserRegistrationView(APIView):
+    def post(self,request,format=None):
+        serializer = UserRegistrationSerializer(data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            user  = serializer.save()
+            return Response({'msg':' registration success'},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+    def post(self,request,format=None):
+        return Response({'msg':' login success'},status=status.HTTP_200_OK)
