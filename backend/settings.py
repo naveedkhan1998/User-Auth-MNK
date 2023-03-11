4ea8bdb5144dee4258eacda7bc954439922f831d
Naveed Khan <naveed.khan@geniobits.in>
2023-03-11 14:20:37 +0530

    lkgghfdh

--- backend/settings.py
+++ backend/settings.py
@@ -51,8 +51,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     "corsheaders",
-    "User-Auth-MNK.managment",
-    'User-Auth-MNK.account',
+    "managment",
+    'account',
 ]
 
 MIDDLEWARE = [
