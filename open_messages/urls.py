cddec90377490c5b6c92a1302fbe3466091c64a9
nkhan364 <naveedkhan13041998@gmail.com>
2023-09-26 16:16:15 -0400

    ccasd

--- open_messages/urls.py
+++ open_messages/urls.py
@@ -6,6 +6,6 @@ from .views import MessageView,MessageList
 
 urlpatterns = [
     path('',MessageView.as_view(),name='message'),
-    path('<int:pk>',MessageList.as_view(),name='message_delete'),
+    path('<int:pk>/',MessageList.as_view(),name='message_delete'),
 
 ]
