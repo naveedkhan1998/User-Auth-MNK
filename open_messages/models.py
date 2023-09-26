60b3f85cd413bc77ca7ac24e4568252bae292037
nkhan364 <naveedkhan13041998@gmail.com>
2023-09-26 15:53:32 -0400

    ccad

--- open_messages/models.py
+++ open_messages/models.py
@@ -6,8 +6,8 @@ class Message(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField( max_length=254) 
     message = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Name: {self.name} Message: {self.message[:20]}'
+        return f'Name: {self.name} Message: {self.message[:20]} TimeStamp:{self.created_at}'
     
