5426c32075b3a3239b012005b3b7bccb3d8d91a2
nkhan364 <naveedkhan13041998@gmail.com>
2023-09-28 08:43:20 -0400

    change refresh time to 30 mins

--- backend/settings.py
+++ backend/settings.py
@@ -181,7 +181,7 @@ mimetypes.add_type("text/javascript", ".js", True)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
