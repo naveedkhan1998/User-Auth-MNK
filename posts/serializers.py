e5395252585277a5bf513f6e890abeb7fbf21406
nkhan364 <naveedkhan13041998@gmail.com>
2023-10-10 17:39:14 -0400

    serip

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,14 +1,17 @@
 from rest_framework import serializers
 from .models import Posts,Transactions
-
+from account.serializers import UserProfileSerializer
 
 class PostsSerializer(serializers.ModelSerializer):
+    created_by = UserProfileSerializer()
     class Meta:
         model = Posts
         fields = "__all__"
 
         
 class TransactionsSerializer(serializers.ModelSerializer):
+    posts = PostsSerializer()
+    buyer = UserProfileSerializer()
     class Meta:
         model = Transactions
         fields = "__all__"
