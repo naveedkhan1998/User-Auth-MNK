c18e7d345e6fb196ab6e6d529ac7799f788d9673
nkhan364 <naveedkhan13041998@gmail.com>
2023-11-18 11:13:16 -0500

    fix user

--- account/models.py
+++ account/models.py
@@ -1,30 +1,35 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 
 
-#custom model
+# custom model
 class UserManager(BaseUserManager):
-    def create_user(self, email, name,tc,otp, password=None,password2=None):
+    def create_user(self, email, name, tc, otp, password=None, password2=None):
         """
         Creates and saves a User with the given email, name,
         tc and password.
         """
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("Users must have an email address")
 
         user = self.model(
             email=self.normalize_email(email),
             name=name,
             tc=tc,
-            otp = otp,
-            is_email_verify=True
+            otp=otp,
+            is_email_verify=True,
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email,name,tc,otp=12345, password=None,
-    password2=None):
+    def create_superuser(
+        self, email, name, tc, otp=12345, password=None, password2=None
+    ):
         """
         Creates and saves a superuser with the given email, date of
         birth and password.
@@ -34,41 +39,41 @@ class UserManager(BaseUserManager):
             password=password,
             name=name,
             tc=tc,
-            otp = otp,
-            #is_email_verify=True
+            otp=otp,
+            # is_email_verify=True
         )
         user.is_admin = True
         user.save(using=self._db)
         return user
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(
-        verbose_name='Email',
+        verbose_name="Email",
         max_length=255,
         unique=True,
     )
     avatar = models.ImageField(
-        verbose_name='avatar',
-        upload_to='user/avatar/',
+        verbose_name="avatar",
+        upload_to="user/avatar/",
         null=True,
-        default='/profile_icon.png'
+        default="/profile_icon.png",
     )
     name = models.CharField(max_length=200)
     tc = models.BooleanField()
     is_active = models.BooleanField(default=True)
     is_email_verify = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
-    otp = models.IntegerField(null=True,blank=True)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    otp = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_in_session = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name','tc']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name", "tc"]
 
     def __str__(self):
         return self.email
@@ -92,15 +97,15 @@ class User(AbstractBaseUser):
 
 class UserOtps(models.Model):
     email = models.EmailField(
-        verbose_name='Email',
+        verbose_name="Email",
         max_length=255,
         unique=True,
     )
-    otp = models.IntegerField(blank=False,null=False)
+    otp = models.IntegerField(blank=False, null=False)
     attempts = models.IntegerField(default=5)
     is_used = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.email
