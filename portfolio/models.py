71697f40ff0402f2db701aa816b12e4ba7084d14
nkhan364 <naveedkhan13041998@gmail.com>
2024-02-27 12:48:04 -0500

    lol

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,35 @@
-# portfolio/models.py
 from django.db import models
+from PIL import Image as PilImage
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 
 
 class Image(models.Model):
     file = models.ImageField(upload_to="project_images/")
+
+    def save(self, *args, **kwargs):
+        # Open the image using Pillow
+        img = PilImage.open(self.file)
+        # Convert to RGB mode if the image is in RGBA mode
+        if img.mode == "RGBA":
+            img = img.convert("RGB")
+        # Compress the image
+        output = BytesIO()
+        img.save(output, format="JPEG", quality=70)
+        output.seek(0)
+
+        # Set the content of the compressed image to the file field
+        self.file = InMemoryUploadedFile(
+            output,
+            "ImageField",
+            f"{self.file.name.split('.')[0]}.jpg",
+            "image/jpeg",
+            sys.getsizeof(output),
+            None,
+        )
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return str(self.file)
