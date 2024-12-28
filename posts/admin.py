4dadca4f5bab8f5f8e3c481239d509ec8cad15f0
nkhan364 <naveedkhan13041998@gmail.com>
2024-12-28 15:55:27 -0500

    add inventory to admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Posts,Transactions,Item
+from .models import Posts, Transactions, Item, Inventory
+
 # Register your models here.
 admin.site.register(Posts)
 admin.site.register(Transactions)
 admin.site.register(Item)
+admin.site.register(Inventory)
