a5ed99cacaafb4e0bd5e2a86c11ddfd21bae9089
nkhan364 <naveedkhan13041998@gmail.com>
2025-08-21 09:27:48 -0400

    refactor: move load_dotenv call to the top of the settings file

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,7 +14,9 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import mimetypes
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
