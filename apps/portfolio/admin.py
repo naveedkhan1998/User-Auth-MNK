c35339db518d8339036af0ee1c12d973653fdc6a
nkhan364 <naveedkhan13041998@gmail.com>
2025-09-19 23:25:48 -0400

    asds

--- apps/portfolio/admin.py
+++ apps/portfolio/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Project, Image
 
-# Register your models here.
 
-admin.site.register(Project)
-admin.site.register(Image)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'technology_used', 'created_at')
+    search_fields = ('title', 'description', 'technology_used')
+    filter_horizontal = ('images',)  # Allows easy selection of multiple images
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('file',)
